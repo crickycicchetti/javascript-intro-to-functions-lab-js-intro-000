707b4a75a63c7ad30862d13adde46416889e0916
Cricky Cicchetti <cricky@flatironschool.com>
2018-05-16 20:49:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toUpperCase === string) {
+  if (string.toLowerCase) {
     return "I can't hear you!"
   }
   else if (string === "I love you, Grandma."){
